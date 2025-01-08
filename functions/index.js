0e44c3167959df4ea90d8cf92fa4259a643a9e57
Tony Mak <tony_mak@smartone.com>
2025-01-08 14:39:14 +0800

    initial commit

---
+++ functions/index.js
@@ -0,0 +1,9 @@
+export async function onRequestGet({ request, next }) {
+    const url = new URL(request.url);
+    const query = Object.fromEntries(url.searchParams);
+    if (query.status == "success") {
+        url.pathname = "/success"
+        return Response.redirect(url, 302);
+    }
+    return next();
+}
